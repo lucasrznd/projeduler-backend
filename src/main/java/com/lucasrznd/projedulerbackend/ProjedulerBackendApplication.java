bc1dedddeb335125deebcaa8436be269f88f750f
Lucas Rezende <lucasffrezende@gmail.com>
2025-01-30 10:51:46 -0300

    chore: create project structure

---
+++ src/main/java/com/lucasrznd/projedulerbackend/ProjedulerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lucasrznd.projedulerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjedulerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjedulerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucasrznd/projedulerbackend/ProjedulerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasrznd.projedulerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjedulerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
