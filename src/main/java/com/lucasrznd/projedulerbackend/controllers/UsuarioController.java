4fd1631c2a8e8bbb7abe38d489de7c88f8090848
Lucas Rezende <lucasffrezende@gmail.com>
2025-01-30 14:12:29 -0300

    feat(user): add controller

---
+++ src/main/java/com/lucasrznd/projedulerbackend/controllers/UsuarioController.java
@@ -0,0 +1,78 @@
+package com.lucasrznd.projedulerbackend.controllers;
+
+import com.lucasrznd.projedulerbackend.dtos.request.UsuarioRequest;
+import com.lucasrznd.projedulerbackend.dtos.response.UsuarioResponse;
+import com.lucasrznd.projedulerbackend.exceptions.StandardError;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Tag(name = "UsuarioController", description = "Controller responsavél pelas operações de Usuarios")
+@RequestMapping("/usuarios")
+public interface UsuarioController {
+
+    @Operation(summary = "Criar novo usuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuario criado", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = UsuarioResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class)))
+    })
+    @PostMapping
+    ResponseEntity<UsuarioResponse> create(@RequestBody @Valid final UsuarioRequest request);
+
+    @Operation(summary = "Buscar todos usuarios")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuarios encontrados", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = UsuarioResponse.class)))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class)))
+    })
+    @GetMapping
+    ResponseEntity<List<UsuarioResponse>> findAll();
+
+    @Operation(summary = "Atualizar usuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario atualizado", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = UsuarioResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class))),
+            @ApiResponse(responseCode = "404", description = "Usuario não encontrado", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class)))
+    })
+    @PutMapping("/{id}")
+    ResponseEntity<UsuarioResponse> update(@Parameter(description = "Usuario Id", required = true, example = "1")
+                                           @PathVariable(name = "id") Long id,
+                                           @RequestBody @Valid UsuarioRequest request);
+
+    @Operation(summary = "Remover usuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Usuario removido"),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class))),
+            @ApiResponse(responseCode = "404", description = "Usuario não encontrado", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = StandardError.class))))
+    })
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> delete(@Parameter(description = "Usuario Id", required = true, example = "1")
+                                @PathVariable(name = "id") Long id);
+
+}
---
+++ src/main/java/com/lucasrznd/projedulerbackend/controllers/impl/UsuarioControllerImpl.java
@@ -0,0 +1,42 @@
+package com.lucasrznd.projedulerbackend.controllers.impl;
+
+import com.lucasrznd.projedulerbackend.controllers.UsuarioController;
+import com.lucasrznd.projedulerbackend.dtos.request.UsuarioRequest;
+import com.lucasrznd.projedulerbackend.dtos.response.UsuarioResponse;
+import com.lucasrznd.projedulerbackend.services.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequiredArgsConstructor
+public class UsuarioControllerImpl implements UsuarioController {
+
+    private final UsuarioService service;
+
+    @Override
+    public ResponseEntity<UsuarioResponse> create(UsuarioRequest request) {
+        return ResponseEntity.status(CREATED).body(service.create(request));
+    }
+
+    @Override
+    public ResponseEntity<List<UsuarioResponse>> findAll() {
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @Override
+    public ResponseEntity<UsuarioResponse> update(Long id, UsuarioRequest request) {
+        return ResponseEntity.ok().body(service.update(id, request));
+    }
+
+    @Override
+    public ResponseEntity<Void> delete(Long id) {
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
