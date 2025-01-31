c3fb6820936f5d7bc6d0836b39125002c3f59b5a
Lucas Rezende <lucasffrezende@gmail.com>
2025-01-31 08:38:15 -0300

    feat: add spring security

--- src/main/java/com/lucasrznd/projedulerbackend/configs/OpenAPIConfig.java
+++ src/main/java/com/lucasrznd/projedulerbackend/configs/OpenAPIConfig.java
@@ -1,4 +1,4 @@
-package com.lucasrznd.projedulerbackend.config;
+package com.lucasrznd.projedulerbackend.configs;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
