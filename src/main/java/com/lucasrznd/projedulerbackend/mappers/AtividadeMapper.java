08261954ea275367930c276a6d86b0b1fc695a74
Lucas Rezende <lucasffrezende@gmail.com>
2025-01-31 18:31:59 -0300

    feat(taskMapper): add custom mapping to fields

--- src/main/java/com/lucasrznd/projedulerbackend/mappers/AtividadeMapper.java
+++ src/main/java/com/lucasrznd/projedulerbackend/mappers/AtividadeMapper.java
@@ -3,9 +3,11 @@ package com.lucasrznd.projedulerbackend.mappers;
 import com.lucasrznd.projedulerbackend.dtos.request.AtividadeRequest;
 import com.lucasrznd.projedulerbackend.dtos.response.AtividadeResponse;
 import com.lucasrznd.projedulerbackend.models.Atividade;
+import com.lucasrznd.projedulerbackend.models.Projeto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
@@ -18,8 +20,22 @@ public interface AtividadeMapper {
     AtividadeResponse toResponse(final Atividade atividade);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "projeto.id", source = "projetoId")
+    @Mapping(target = "usuarioResponsavel.id", source = "usuarioResponsavelId")
     Atividade toModel(final AtividadeRequest atividadeRequest);
 
+    @Mapping(target = "projeto", source = "projetoId", qualifiedByName = "mapProjeto")
+    @Mapping(target = "usuarioResponsavel", source = "usuarioResponsavelId", qualifiedByName = "mapUsuarioResponsavel")
     Atividade update(final AtividadeRequest atividadeRequest, @MappingTarget Atividade atividade);
+
+    @Named("mapProjeto")
+    default Projeto mapProjeto(Long projetoId) {
+        if (projetoId == null) {
+            return null;
+        }
+        Projeto projeto = new Projeto();
+        projeto.setId(projetoId);
+        return projeto;
+    }
 
 }
