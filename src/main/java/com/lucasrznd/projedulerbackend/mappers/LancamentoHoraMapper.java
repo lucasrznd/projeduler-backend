b28c44fef8f5cccebb6ac87fcd4faa5ba0963e85
Lucas Rezende <lucasffrezende@gmail.com>
2025-01-31 18:51:08 -0300

    feat(taskLog): add custom mapping to fields

--- src/main/java/com/lucasrznd/projedulerbackend/mappers/LancamentoHoraMapper.java
+++ src/main/java/com/lucasrznd/projedulerbackend/mappers/LancamentoHoraMapper.java
@@ -2,10 +2,12 @@ package com.lucasrznd.projedulerbackend.mappers;
 
 import com.lucasrznd.projedulerbackend.dtos.request.LancamentoHoraRequest;
 import com.lucasrznd.projedulerbackend.dtos.response.LancamentoHoraResponse;
+import com.lucasrznd.projedulerbackend.models.Atividade;
 import com.lucasrznd.projedulerbackend.models.LancamentoHora;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
@@ -18,8 +20,22 @@ public interface LancamentoHoraMapper {
     LancamentoHoraResponse toResponse(final LancamentoHora lancamentoHora);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "atividade.id", source = "atividadeId")
+    @Mapping(target = "usuario.id", source = "usuarioId")
     LancamentoHora toModel(final LancamentoHoraRequest lancamentoHoraRequest);
 
+    @Mapping(target = "atividade", source = "atividadeId", qualifiedByName = "mapAtividade")
+    @Mapping(target = "usuario", source = "usuarioId", qualifiedByName = "mapUsuarioResponsavel")
     LancamentoHora update(final LancamentoHoraRequest request, @MappingTarget LancamentoHora lancamentoHora);
+
+    @Named("mapAtividade")
+    default Atividade mapAtividade(Long atividadeId) {
+        if (atividadeId == null) {
+            return null;
+        }
+        Atividade atividade = new Atividade();
+        atividade.setId(atividadeId);
+        return atividade;
+    }
 
 }
