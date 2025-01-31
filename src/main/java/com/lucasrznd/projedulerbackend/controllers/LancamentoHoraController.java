4db3956c3b4511359df0dbae8e42b9d11a7cd85e
Lucas Rezende <lucasffrezende@gmail.com>
2025-01-31 18:51:34 -0300

    feat(taskLog): add controller

---
+++ src/main/java/com/lucasrznd/projedulerbackend/controllers/LancamentoHoraController.java
@@ -0,0 +1,78 @@
+package com.lucasrznd.projedulerbackend.controllers;
+
+import com.lucasrznd.projedulerbackend.dtos.request.LancamentoHoraRequest;
+import com.lucasrznd.projedulerbackend.dtos.response.LancamentoHoraResponse;
+import com.lucasrznd.projedulerbackend.exceptions.StandardError;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Tag(name = "LancamentoHoraController", description = "Controller responsavél pelas operações de lançamento de hora")
+@RequestMapping("/lancamentos-horas")
+public interface LancamentoHoraController {
+
+    @Operation(summary = "Criar novo lançamento")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Lançamento criado", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = LancamentoHoraResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class)))
+    })
+    @PostMapping
+    ResponseEntity<LancamentoHoraResponse> create(@RequestBody @Valid final LancamentoHoraRequest request);
+
+    @Operation(summary = "Buscar todos lançamentos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lançamentos encontrados", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = LancamentoHoraResponse.class)))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class)))
+    })
+    @GetMapping
+    ResponseEntity<List<LancamentoHoraResponse>> findAll();
+
+    @Operation(summary = "Atualizar lançamento")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lançamento atualizado", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = LancamentoHoraResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class))),
+            @ApiResponse(responseCode = "404", description = "Lançamento não encontrado", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class)))
+    })
+    @PutMapping("/{id}")
+    ResponseEntity<LancamentoHoraResponse> update(@Parameter(description = "Lançamento Id", required = true, example = "1")
+                                                  @PathVariable(name = "id") Long id,
+                                                  @RequestBody @Valid LancamentoHoraRequest request);
+
+    @Operation(summary = "Remover lançamento")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Lançamento removido"),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class))),
+            @ApiResponse(responseCode = "404", description = "Lançamento não encontrado", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = StandardError.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(
+                    mediaType = APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = StandardError.class))))
+    })
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> delete(@Parameter(description = "Lançamento Id", required = true, example = "1")
+                                @PathVariable(name = "id") Long id);
+
+}
---
+++ src/main/java/com/lucasrznd/projedulerbackend/controllers/impl/LancamentoHoraControllerImpl.java
@@ -0,0 +1,42 @@
+package com.lucasrznd.projedulerbackend.controllers.impl;
+
+import com.lucasrznd.projedulerbackend.controllers.LancamentoHoraController;
+import com.lucasrznd.projedulerbackend.dtos.request.LancamentoHoraRequest;
+import com.lucasrznd.projedulerbackend.dtos.response.LancamentoHoraResponse;
+import com.lucasrznd.projedulerbackend.services.LancamentoHoraService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequiredArgsConstructor
+public class LancamentoHoraControllerImpl implements LancamentoHoraController {
+
+    private final LancamentoHoraService service;
+
+    @Override
+    public ResponseEntity<LancamentoHoraResponse> create(LancamentoHoraRequest request) {
+        return ResponseEntity.status(CREATED).body(service.save(request));
+    }
+
+    @Override
+    public ResponseEntity<List<LancamentoHoraResponse>> findAll() {
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @Override
+    public ResponseEntity<LancamentoHoraResponse> update(Long id, LancamentoHoraRequest request) {
+        return ResponseEntity.ok().body(service.update(id, request));
+    }
+
+    @Override
+    public ResponseEntity<Void> delete(Long id) {
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
