461f6eb4699ee8a7cc2db8b14143825d93e29e6b
Lucas Rezende <lucasffrezende@gmail.com>
2025-02-01 08:55:59 -0300

    feat(projectUser): add custom mapping to fields

--- src/main/java/com/lucasrznd/projedulerbackend/mappers/UsuarioProjetoMapper.java
+++ src/main/java/com/lucasrznd/projedulerbackend/mappers/UsuarioProjetoMapper.java
@@ -3,23 +3,23 @@ package com.lucasrznd.projedulerbackend.mappers;
 import com.lucasrznd.projedulerbackend.dtos.request.UsuarioProjetoRequest;
 import com.lucasrznd.projedulerbackend.dtos.response.UsuarioProjetoResponse;
 import com.lucasrznd.projedulerbackend.models.UsuarioProjeto;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValueCheckStrategy;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring",
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
         nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
-        uses = {ProjetoMapper.class, UsuarioMapper.class})
+        uses = {ProjetoMapper.class, AtividadeMapper.class})
 public interface UsuarioProjetoMapper {
 
     UsuarioProjetoResponse toResponse(final UsuarioProjeto usuarioProjeto);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "usuario.id", source = "usuarioId")
+    @Mapping(target = "projeto.id", source = "projetoId")
     UsuarioProjeto toModel(final UsuarioProjetoRequest usuarioProjetoRequest);
 
+    @Mapping(target = "usuario", source = "usuarioId", qualifiedByName = "mapUsuarioResponsavel")
+    @Mapping(target = "projeto", source = "projetoId", qualifiedByName = "mapProjeto")
     UsuarioProjeto update(final UsuarioProjetoRequest request, @MappingTarget UsuarioProjeto usuarioProjeto);
 
 }
