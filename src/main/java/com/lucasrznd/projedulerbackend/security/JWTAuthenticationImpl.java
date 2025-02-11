720244c2067f3670a8f87111d2a818276fd994f8
Lucas Rezende <lucasffrezende@gmail.com>
2025-02-11 10:22:55 -0300

    feat(jwtAuthentication): add last login and javadoc for methods

--- src/main/java/com/lucasrznd/projedulerbackend/security/JWTAuthenticationImpl.java
+++ src/main/java/com/lucasrznd/projedulerbackend/security/JWTAuthenticationImpl.java
@@ -14,6 +14,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Log4j2
 @Service
 public class JWTAuthenticationImpl {
@@ -34,6 +36,18 @@ public class JWTAuthenticationImpl {
         this.repository = repository;
     }
 
+    /**
+     * Realiza a autenticação do usuário e gera tokens JWT de acesso e refresh.
+     * <p>
+     * O método valida as credenciais do usuário, gerando um token de acesso e um refresh token
+     * para a sessão autenticada. Após a autenticação bem-sucedida, a data do último login do usuário
+     * é atualizada no banco de dados.
+     * </p>
+     *
+     * @param request Objeto {@link AuthenticateRequest} contendo o email e senha do usuário.
+     * @return {@link AuthenticationResponse} contendo os tokens JWT gerados para o usuário.
+     * @throws BadCredentialsException Se as credenciais do usuário forem inválidas.
+     */
     public AuthenticationResponse login(AuthenticateRequest request) {
         try {
             var usernamePassword = new UsernamePasswordAuthenticationToken(request.email(), request.senha());
@@ -41,6 +55,8 @@ public class JWTAuthenticationImpl {
 
             String token = jwtUtils.generateToken((Usuario) auth.getPrincipal(), accessTokenExpiration);
             String refreshToken = jwtUtils.generateToken((Usuario) auth.getPrincipal(), refreshTokenExpiration);
+
+            updateLastLogin((Usuario) auth.getPrincipal());
 
             log.info("Usuário autenticado com sucesso: {}", request.email());
             return new AuthenticationResponse(token, refreshToken);
@@ -50,13 +66,34 @@ public class JWTAuthenticationImpl {
         }
     }
 
+    /**
+     * Gera um novo token de acesso utilizando um refresh token válido.
+     * <p>
+     * O método valida o refresh token fornecido, recupera o usuário associado e gera um novo token de acesso.
+     * Após isso, o último login do usuário é atualizado no banco de dados. O refresh token permanece válido,
+     * permitindo que o mesmo seja retornado na resposta.
+     * </p>
+     *
+     * @param refreshToken O token de refresh fornecido pelo cliente para gerar um novo token de acesso.
+     * @return {@link AuthenticationResponse} contendo o novo token de acesso gerado e o refresh token original.
+     * @throws UsernameNotFoundException Se o usuário associado ao refresh token não for encontrado no banco de dados.
+     */
     public AuthenticationResponse refreshToken(String refreshToken) {
         String login = jwtUtils.validateToken(refreshToken);
-        Usuario usuario = repository.findByEmail(login).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado."));
+        Usuario usuario = repository.findByEmail(login)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado."));
 
         var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        updateLastLogin(usuario);
+
         return new AuthenticationResponse(jwtUtils.generateToken(usuario, accessTokenExpiration), refreshToken);
+    }
+
+    private void updateLastLogin(Usuario usuario) {
+        usuario.setUltimoLogin(LocalDateTime.now());
+        repository.save(usuario);
     }
 }
