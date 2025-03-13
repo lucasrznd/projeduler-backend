49c3860340349251073efc4eb0c9d9019cedbc27
Lucas Rezende <lucasffrezende@gmail.com>
2025-03-13 09:57:09 -0300

    feat(openAPI): add authorization config on swagger ui

--- src/main/java/com/lucasrznd/projedulerbackend/configs/OpenAPIConfig.java
+++ src/main/java/com/lucasrznd/projedulerbackend/configs/OpenAPIConfig.java
@@ -2,6 +2,8 @@ package com.lucasrznd.projedulerbackend.configs;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +17,20 @@ public class OpenAPIConfig {
                                  @Value("${springdoc.open-api.version}") final String version) {
         return new OpenAPI()
                 .info(
-                        new Info().title(title).description(description).version(version)
-                );
+                        new Info().title(title)
+                                .description(description)
+                                .version(version))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", securityScheme()));
     }
 
+    public SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+    }
 }
