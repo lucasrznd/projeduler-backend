e3992149c4bbf2173a698a1586742967de31422d
Lucas Rezende <lucasffrezende@gmail.com>
2025-03-14 19:06:56 -0300

    feat(config): add MessageConfig for internationalization support with default locale and message source

---
+++ src/main/java/com/lucasrznd/projedulerbackend/configs/MessageConfig.java
@@ -0,0 +1,29 @@
+package com.lucasrznd.projedulerbackend.configs;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class MessageConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pt", "BR"));
+        return localeResolver;
+    }
+}
