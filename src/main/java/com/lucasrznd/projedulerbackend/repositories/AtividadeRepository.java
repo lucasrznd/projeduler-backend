970ac1ddc8cfa654f8715767dea0d0db0a51728b
Lucas Rezende <lucasffrezende@gmail.com>
2025-03-14 00:15:31 -0300

    feat(atividadeRepository): add method to count activities by status with filtering options

--- src/main/java/com/lucasrznd/projedulerbackend/repositories/AtividadeRepository.java
+++ src/main/java/com/lucasrznd/projedulerbackend/repositories/AtividadeRepository.java
@@ -4,8 +4,10 @@ import com.lucasrznd.projedulerbackend.models.Atividade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -43,5 +45,23 @@ public interface AtividadeRepository extends JpaRepository<Atividade, Long> {
     @Modifying
     @Query("UPDATE Atividade a SET a.ativo = false, a.dataExclusao = :dataExclusao WHERE a.projeto.id = :projetoId")
     void softDeleteAllByProjetoId(Long projetoId, LocalDateTime dataExclusao);
+
+    @Query(value = "SELECT a.status, COUNT(a.id) as quantidade " +
+            "FROM tb_atividade a " +
+            "JOIN tb_projeto p ON a.projeto_id = p.id " +
+            "LEFT JOIN tb_usuario_atividade ua ON a.id = ua.atividade_id " +
+            "WHERE (a.data_inicio >= :dataInicio OR :dataInicio IS NULL) " +
+            "AND (a.data_fim <= :dataFim OR :dataFim IS NULL) " +
+            "AND (:projetoId IS NULL OR p.id = :projetoId) " +
+            "AND (:usuarioId IS NULL OR ua.usuario_id = :usuarioId) " +
+            "AND (:status IS NULL OR a.status = :status) " +
+            "AND p.ativo = 1 AND a.ativo = 1 " +
+            "GROUP BY a.status", nativeQuery = true)
+    List<Object[]> contarAtividadesPorStatus(
+            @Param("dataInicio") LocalDate dataInicio,
+            @Param("dataFim") LocalDate dataFim,
+            @Param("projetoId") Long projetoId,
+            @Param("usuarioId") Long usuarioId,
+            @Param("status") String status);
 
 }
