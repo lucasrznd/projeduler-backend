acf6935f1f2d6b324384f59f1065cc919b8014a5
Lucas Rezende <lucasffrezende@gmail.com>
2025-03-13 21:09:05 -0300

    refactor(lancamentoHoraRepository): add clause to SELECT

--- src/main/java/com/lucasrznd/projedulerbackend/repositories/LancamentoHoraRepository.java
+++ src/main/java/com/lucasrznd/projedulerbackend/repositories/LancamentoHoraRepository.java
@@ -12,37 +12,37 @@ import java.util.Optional;
 @Repository
 public interface LancamentoHoraRepository extends JpaRepository<LancamentoHora, Long> {
 
-    @Query("SELECT lh FROM LancamentoHora lh WHERE lh.usuario.ativo = true ORDER BY lh.dataRegistro DESC")
+    @Query("SELECT lh FROM LancamentoHora lh WHERE lh.usuario.ativo = true AND lh.atividade.ativo = true AND lh.atividade.projeto.ativo = true ORDER BY lh.dataRegistro DESC")
     List<LancamentoHora> findAllLancamentos();
 
-    @Query("SELECT lh FROM LancamentoHora lh  WHERE lh.usuario.ativo = true ORDER BY lh.dataRegistro DESC LIMIT 5")
+    @Query("SELECT lh FROM LancamentoHora lh WHERE lh.usuario.ativo = true AND lh.atividade.ativo = true AND lh.atividade.projeto.ativo = true ORDER BY lh.dataRegistro DESC LIMIT 5")
     List<LancamentoHora> findUltimosCinco();
 
-    @Query("SELECT lh FROM LancamentoHora lh WHERE lh.usuario.id = :usuarioId AND lh.usuario.ativo = true ORDER BY lh.dataRegistro DESC LIMIT 5")
+    @Query("SELECT lh FROM LancamentoHora lh WHERE lh.usuario.id = :usuarioId AND lh.usuario.ativo = true  AND lh.atividade.ativo = true AND lh.atividade.projeto.ativo = true ORDER BY lh.dataRegistro DESC LIMIT 5")
     List<LancamentoHora> findUltimosCincoByUsuarioId(Long usuarioId);
 
-    @Query("SELECT lh FROM LancamentoHora lh WHERE lh.usuario.id = :usuarioId AND lh.usuario.ativo = true  ORDER BY lh.dataRegistro DESC")
+    @Query("SELECT lh FROM LancamentoHora lh WHERE lh.usuario.id = :usuarioId AND lh.usuario.ativo = true AND lh.atividade.ativo = true AND lh.atividade.projeto.ativo = true ORDER BY lh.dataRegistro DESC")
     List<LancamentoHora> findByUsuarioId(Long usuarioId);
 
-    @Query("SELECT lh FROM LancamentoHora lh WHERE lh.dataInicio = :dataInicio AND lh.dataFim = :dataFim AND lh.usuario.id = :usuarioId AND lh.usuario.ativo = true")
+    @Query("SELECT lh FROM LancamentoHora lh WHERE lh.dataInicio = :dataInicio AND lh.dataFim = :dataFim AND lh.usuario.id = :usuarioId AND lh.usuario.ativo = true AND lh.atividade.ativo = true AND lh.atividade.projeto.ativo = true")
     Optional<LancamentoHora> findByDataInicioAndDataFimAndUsuarioId(LocalDateTime dataInicio, LocalDateTime dataFim, Long usuarioId);
 
-    @Query("SELECT lh FROM LancamentoHora lh WHERE MONTH(lh.dataRegistro) = MONTH(CURRENT_DATE) AND YEAR(lh.dataRegistro) = YEAR(CURRENT_DATE) AND lh.usuario.ativo = true ")
+    @Query("SELECT lh FROM LancamentoHora lh WHERE MONTH(lh.dataRegistro) = MONTH(CURRENT_DATE) AND YEAR(lh.dataRegistro) = YEAR(CURRENT_DATE) AND lh.usuario.ativo = true AND lh.atividade.ativo = true AND lh.atividade.projeto.ativo = true")
     List<LancamentoHora> findLancamentosMesAtual();
 
-    @Query("SELECT lh FROM LancamentoHora lh WHERE MONTH(lh.dataRegistro) = MONTH(CURRENT_DATE) AND YEAR(lh.dataRegistro) = YEAR(CURRENT_DATE) AND lh.usuario.id = :usuarioId AND lh.usuario.ativo = true ")
+    @Query("SELECT lh FROM LancamentoHora lh WHERE MONTH(lh.dataRegistro) = MONTH(CURRENT_DATE) AND YEAR(lh.dataRegistro) = YEAR(CURRENT_DATE) AND lh.usuario.id = :usuarioId AND lh.usuario.ativo = true  AND lh.atividade.ativo = true AND lh.atividade.projeto.ativo = true")
     List<LancamentoHora> findLancamentosMesAtualByUsuarioId(Long usuarioId);
 
-    @Query("SELECT lh FROM LancamentoHora lh WHERE WEEK(lh.dataRegistro) = WEEK(CURRENT_DATE) AND YEAR(lh.dataRegistro) = YEAR(CURRENT_DATE) AND MONTH(lh.dataRegistro) = MONTH(CURRENT_DATE) AND lh.usuario.ativo = true ")
+    @Query("SELECT lh FROM LancamentoHora lh WHERE WEEK(lh.dataRegistro) = WEEK(CURRENT_DATE) AND YEAR(lh.dataRegistro) = YEAR(CURRENT_DATE) AND MONTH(lh.dataRegistro) = MONTH(CURRENT_DATE) AND lh.usuario.ativo = true AND lh.atividade.ativo = true AND lh.atividade.projeto.ativo = true")
     List<LancamentoHora> findLancamentosSemanaAtual();
 
-    @Query("SELECT lh FROM LancamentoHora lh WHERE WEEK(lh.dataRegistro) = WEEK(CURRENT_DATE) AND YEAR(lh.dataRegistro) = YEAR(CURRENT_DATE) AND MONTH(lh.dataRegistro) = MONTH(CURRENT_DATE) AND lh.usuario.id = :usuarioId AND lh.usuario.ativo = true ")
+    @Query("SELECT lh FROM LancamentoHora lh WHERE WEEK(lh.dataRegistro) = WEEK(CURRENT_DATE) AND YEAR(lh.dataRegistro) = YEAR(CURRENT_DATE) AND MONTH(lh.dataRegistro) = MONTH(CURRENT_DATE) AND lh.usuario.id = :usuarioId AND lh.usuario.ativo = true AND lh.atividade.ativo = true AND lh.atividade.projeto.ativo = true")
     List<LancamentoHora> findLancamentosSemanaAtualByUsuarioId(Long usuarioId);
 
-    @Query("SELECT lh FROM LancamentoHora lh WHERE DAY(lh.dataRegistro) = DAY(CURRENT_DATE) AND YEAR(lh.dataRegistro) = YEAR(CURRENT_DATE) AND MONTH(lh.dataRegistro) = MONTH(CURRENT_DATE) AND lh.usuario.ativo = true ")
+    @Query("SELECT lh FROM LancamentoHora lh WHERE DAY(lh.dataRegistro) = DAY(CURRENT_DATE) AND YEAR(lh.dataRegistro) = YEAR(CURRENT_DATE) AND MONTH(lh.dataRegistro) = MONTH(CURRENT_DATE) AND lh.usuario.ativo = true AND lh.atividade.ativo = true AND lh.atividade.projeto.ativo = true")
     List<LancamentoHora> findLancamentosDiaAtual();
 
-    @Query("SELECT lh FROM LancamentoHora lh WHERE DAY(lh.dataRegistro) = DAY(CURRENT_DATE) AND YEAR(lh.dataRegistro) = YEAR(CURRENT_DATE) AND MONTH(lh.dataRegistro) = MONTH(CURRENT_DATE) AND lh.usuario.id = :usuarioId AND lh.usuario.ativo = true ")
+    @Query("SELECT lh FROM LancamentoHora lh WHERE DAY(lh.dataRegistro) = DAY(CURRENT_DATE) AND YEAR(lh.dataRegistro) = YEAR(CURRENT_DATE) AND MONTH(lh.dataRegistro) = MONTH(CURRENT_DATE) AND lh.usuario.id = :usuarioId AND lh.usuario.ativo = true AND lh.atividade.ativo = true AND lh.atividade.projeto.ativo = true")
     List<LancamentoHora> findLancamentosDiaAtualByUsuarioId(Long usuarioId);
 
 }
